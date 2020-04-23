fe83ea83169102c044ea5d70f61e0bc673bbb9dc
Kristi Gribble <kgribble4083@gmail.com>
2020-04-22 21:03:26 -0500

    user-router get complete

--- user/user-router.js
+++ user/user-router.js
@@ -4,4 +4,13 @@ const Users = require("./user-model.js")
 
 router.get("/", (req, res) => {
     Users.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "error retrieving users", err })
+        })
 })
+
+
+module.exports = router;
